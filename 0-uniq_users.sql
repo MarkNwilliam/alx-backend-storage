3846ad5fe078972ae63ee47126fc7fcf00a76190
mac <mac@macs-MacBook-Pro.local>
2023-08-11 01:29:49 +0300

    487

--- 0-uniq_users.sql
+++ 0-uniq_users.sql
@@ -1,8 +1,3 @@
--- SQL script to create a table users with specific attributes
--- id: integer, never null, auto increment, primary key
--- email: string (255 characters), never null, unique
--- name: string (255 characters)
-
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
