b16f35042d568ee77b6d2c2e9ed59d814e45c660
mac <mac@macs-MacBook-Pro.local>
2023-08-11 01:51:19 +0300

    cia

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,22 @@
+DELIMITER //
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+  DECLARE done INT DEFAULT 0;
+  DECLARE u_id INT;
+  DECLARE user_cursor CURSOR FOR SELECT id FROM users;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+  OPEN user_cursor;
+  read_loop: LOOP
+    FETCH user_cursor INTO u_id;
+    IF done THEN
+      LEAVE read_loop;
+    END IF;
+
+    CALL ComputeAverageWeightedScoreForUser(u_id);
+  END LOOP;
+
+  CLOSE user_cursor;
+END;
+//
+DELIMITER ;
